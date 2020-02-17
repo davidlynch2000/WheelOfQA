e0c1a50eea63c3ad72f9b1984ab340198df55da7
David Lynch <davidlynch2000@yahoo.com>
2020-02-17 21:36:58 +0100

    so ugly, but so done

--- src/App.js
+++ src/App.js
@@ -1,26 +1,277 @@
-import React from 'react';
+import React, { Component, useState, useRef } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 import logo from './logo.svg';
 import './App.css';
+import Wheel from './wheel';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         {/* <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a> */}
+
+//         found something
+//       </header>
+//     </div>
+//   );
+// }
+
+// fake data generator
+// const getItems = (count, offset = 0) =>
+//     Array.from({ length: count }, (v, k) => k).map(k => ({
+//         id: `item-${k + offset}`,
+//         content: `item ${k + offset}`
+//     }));
+
+const getItems = () => {
+  return [
+    {
+      id: 'Mariam',
+      content: 'Mariam'
+    },
+    {
+      id: 'Josses',
+      content: 'Josses'
+    },
+    {
+      id: 'Jide',
+      content: 'Jide'
+    },
+    {
+      id: 'Kartik',
+      content: 'Kartik'
+    },
+    {
+      id: 'Matt',
+      content: 'Matt'
+    },
+    {
+      id: 'David',
+      content: 'David'
+    },
+    {
+      id: 'Garret',
+      content: 'Garret'
+    },
+    {
+      id: 'Deb',
+      content: 'Deb'
+    },
+  ];
+}
+
+// a little function to help us with reordering the result
+const reorder = (list, startIndex, endIndex) => {
+  const result = Array.from(list);
+  const [removed] = result.splice(startIndex, 1);
+  result.splice(endIndex, 0, removed);
+
+  return result;
+};
+
+/**
+ * Moves an item from one list to another list.
+ */
+const move = (source, destination, droppableSource, droppableDestination) => {
+  const sourceClone = Array.from(source);
+  const destClone = Array.from(destination);
+  const [removed] = sourceClone.splice(droppableSource.index, 1);
+
+  destClone.splice(droppableDestination.index, 0, removed);
+
+  const result = {};
+  result[droppableSource.droppableId] = sourceClone;
+  result[droppableDestination.droppableId] = destClone;
+
+  return result;
+};
+
+const grid = 8;
+
+const getItemStyle = (isDragging, draggableStyle) => ({
+  // some basic styles to make the items look a bit nicer
+  userSelect: 'none',
+  padding: grid * 2,
+  margin: `0 0 ${grid}px 0`,
+
+  // change background colour if dragging
+  background: isDragging ? 'lightgreen' : 'grey',
+
+  // styles we need to apply on draggables
+  ...draggableStyle
+});
+
+const getListStyle = isDraggingOver => ({
+  background: isDraggingOver ? 'lightblue' : 'lightgrey',
+  padding: grid,
+  width: 250
+});
+
+
+
+const App = () => {
+  const [teamMembers, setTeamMembers] = useState(getItems());
+  const [availableForQA, setAvailableForQA] = useState([]);
+  const [selectedForQA, setSelectedForQA] = useState('');
+  const [foundWinner, setFoundWinner] = useState(false);
+
+
+  const popUpWinningModal = (winner) => {
+    setFoundWinner(true);
+  }
+  /**
+   * A semi-generic way to handle multiple lists. Matches
+   * the IDs of the droppable container to the names of the
+   * source arrays stored in the state.
+   */
+  const id2List = {
+    droppable: availableForQA,
+    droppable2: teamMembers,
+  };
+
+  const getList = id => id2List[id];
+
+  const onDragEnd = result => {
+    const { source, destination } = result;
+
+    // dropped outside the list
+    if (!destination) {
+      return;
+    }
+
+    // reordering items in the same list
+    if (source.droppableId === destination.droppableId) {
+      const items = reorder(
+        getList(source.droppableId),//droppableID2
+        source.index,//
+        destination.index
+      );
+
+      if (source.droppableId === 'droppable2') {
+        setTeamMembers(items)
+      }
+      else {
+        setAvailableForQA(items);
+      }
+
+    } else {
+      const result = move(
+        getList(source.droppableId),
+        getList(destination.droppableId),
+        source,
+        destination
+      );
+
+      setAvailableForQA(result.droppable);
+      setTeamMembers(result.droppable2);
+    }
+  };
+
+
+  const closeIt = () => {
+    setFoundWinner(false);
+  }
+
+  const flexClass = foundWinner ? 'displayFlex': '';
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <dialog className={`announceTheWinner ${flexClass}`} open={foundWinner} onClick={closeIt}>
+        <div className='announce'>The Winner is </div>
+        <div className='nameOfWinner'>{selectedForQA}</div>
+      </dialog>
+
+      <h1 className='headerBanner'>Wheel of QA!</h1>
+      <div className='pageContainer'>
+        <DragDropContext onDragEnd={onDragEnd} className='DAndD'>
+          <div className='columnsOfNames'>
+            <h2>Team Members</h2>
+            <div>
+              <Droppable droppableId="droppable2">
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    style={getListStyle(snapshot.isDraggingOver)}>
+                    {teamMembers.map((item, index) => (
+                      <Draggable
+                        key={item.id}
+                        draggableId={item.id}
+                        index={index}>
+                        {(provided, snapshot) => (
+                          <div
+                            ref={provided.innerRef}
+                            {...provided.draggableProps}
+                            {...provided.dragHandleProps}
+                            style={getItemStyle(
+                              snapshot.isDragging,
+                              provided.draggableProps.style
+                            )}>
+                            {item.content}
+                          </div>
+                        )}
+                      </Draggable>
+                    ))}
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </div>
+          </div>
+          <div className='columnsOfNames'>
+            <h2>Available for QA</h2>
+            <Droppable droppableId="droppable">
+              {(provided, snapshot) => (
+                <div
+                  ref={provided.innerRef}
+                  style={getListStyle(snapshot.isDraggingOver)}>
+                  {availableForQA.map((item, index) => (
+                    <Draggable
+                      key={item.id}
+                      draggableId={item.id}
+                      index={index}>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          {...provided.draggableProps}
+                          {...provided.dragHandleProps}
+                          style={getItemStyle(
+                            snapshot.isDragging,
+                            provided.draggableProps.style
+                          )}>
+                          {item.content}
+                        </div>
+                      )}
+                    </Draggable>
+                  ))}
+                  {provided.placeholder}
+                </div>
+              )}
+            </Droppable>
+          </div>
+        </DragDropContext>
+
+
+        <Wheel
+          items={availableForQA.map(teamMember => teamMember.id)}
+          setSelectedForQA={setSelectedForQA}
+          popUpWinningModal={popUpWinningModal}
+        />
+      </div>
     </div>
   );
+
 }
 
 export default App;
---
+++ src/wheel.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef } from 'react';
+import './wheel.css';
+
+const Wheel = (props) => {
+    const [selectedItemIdx, setSelectedItemIdx] = useState(undefined);
+    const spinning = useRef('');
+    const numTotalSpins = useRef(5);
+    const selectedName = useRef('');
+    
+    const SECONDS_TO_SPIN = 6;
+
+    const reset = () => {
+        spinning.current = ('');
+        props.popUpWinningModal(selectedName.current);
+    }
+
+    const selectItem = () => {
+        spinning.current = 'spinning';
+        const selectedIdx = Math.floor(Math.random() * props.items.length);
+        console.log(`selectedIdx: ${selectedIdx}`);
+        props.setSelectedForQA(props.items[selectedIdx]);
+        selectedName.current = props.items[selectedIdx];
+        setSelectedItemIdx(selectedIdx);
+        setTimeout(reset, SECONDS_TO_SPIN * 1000 * 1.001);
+    }
+
+    const { items } = props;
+
+    const wheelVars = {
+        '--nb-item': items.length,
+        '--selected-item': selectedItemIdx,
+        '--nb-turn': numTotalSpins.current,
+        '--spinning-duration': `${SECONDS_TO_SPIN}s`,
+    };
+
+
+    return (
+        <div className="wheel-container">
+            <div className={`wheel ${spinning.current}`} style={wheelVars} onClick={selectItem}>
+                {items.map((item, index) => (
+                    <div className="wheel-item" key={index} style={{ '--item-nb': index }}>
+                        {item}
+                    </div>
+                ))}
+            </div>
+            <div className="buttonContainer">
+                <button type='button' onClick={selectItem}>Spin It!</button>
+            </div>
+        </div>
+    );
+}
+
+
+export default Wheel;
