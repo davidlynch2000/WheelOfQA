8f4db679267903502d2ea837bd6a615bec60eaa7
David Lynch <davidlynch2000@yahoo.com>
2020-02-23 10:43:36 +0100

    Updates

---
+++ precache-manifest.d30690f76c60f001cc66500d50c63696.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e09436b96abb1d7fa10e8d42be448e9",
+    "url": "/WheelOfQA/index.html"
+  },
+  {
+    "revision": "86527ed4164a397d7d11",
+    "url": "/WheelOfQA/static/css/main.615c53da.chunk.css"
+  },
+  {
+    "revision": "1479abf71eb3330d6fa9",
+    "url": "/WheelOfQA/static/js/2.74bf7021.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/WheelOfQA/static/js/2.74bf7021.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86527ed4164a397d7d11",
+    "url": "/WheelOfQA/static/js/main.540edc7d.chunk.js"
+  },
+  {
+    "revision": "9e5eb868dc70a0e516b5",
+    "url": "/WheelOfQA/static/js/runtime-main.07dec595.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/WheelOfQA/static/media/logo.5d5d9eef.svg"
+  }
+]);
