fdb2163a63e1b7e14b14a89199bc74d8403fb10a
David Lynch <davidlynch2000@yahoo.com>
2020-03-20 15:09:46 +0100

    cleaning up some of the stuff I copied

--- src/App.js
+++ src/App.js
@@ -42,13 +42,11 @@ const getItems = () => {
   ];
 }
 
-// a little function to help us with reordering the result
+// minimal edits to the list in place
 const reorder = (list, startIndex, endIndex) => {
-  const result = Array.from(list);
-  const [removed] = result.splice(startIndex, 1);
-  result.splice(endIndex, 0, removed);
-
-  return result;
+  const [removed] = list.splice(startIndex, 1);
+  list.splice(endIndex, 0, removed);
+  return list;
 };
 
 /**
